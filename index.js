10abd7a343ffe6397e046acb498ff61fc24bffc0
vcontractor <phindi.mkhize@vcontractor.co.za>
2023-08-29 23:59:51 +0200

    refactored to vanilla js app

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const http = require("http");
 const server = http.createServer(app);
-var io = require("socket.io")(server);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const port = process.env.PORT || 3000;
-
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html", "js");
+// });
 app.use(express.static("dist"));
 
 io.on("connection", (socket) => {
-  io.emit("connection");
+  console.log("a user connected");
+  socket.emit("connected");
+
   socket.on("note played", (note) => {
     io.emit("note played", `${note}`);
   });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
-server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
